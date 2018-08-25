00f90a8b2610bb2f00ef2f826c7197ff3180cfda
Rahul T <rtapase@gmail.com>
2018-08-25 10:16:11 +0530

    Added solution
    Added MVC Music Store solution

---
+++ Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+                 
+        public int ArtistId { get; set; }
+        public virtual Artist Artist { get; set; }
+        
+        public int GenreId { get; set; } 
+        public virtual Genre Genre { get; set; }
+
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
